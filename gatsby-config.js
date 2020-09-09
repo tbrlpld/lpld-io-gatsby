4a7e273f42e5b52b3cfa488d827b2b5c3137aeae
Tibor Leupold <tibor@lpld.io>
2020-09-08 17:06:10 -1000

    Set favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,13 +26,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Portfolio of Tibor Leupold',
+        short_name: 'Portfolio',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#FAFAFA',
+        theme_color: '#333333',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/lpld-icon.png' // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
