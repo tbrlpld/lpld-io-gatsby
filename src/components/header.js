76d241773b5a0dda4357bc2cf75c5b18ffd4795f
Tibor Leupold <tibor@lpld.io>
2020-09-08 15:10:11 -1000

    Add minimal header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,26 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import style from './header.module.css'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/#projects'>Projects</Link></li>
+        <li><Link to='/#contact'>Contact</Link></li>
+      </ul>
+    </nav>
   </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  siteTitle: PropTypes.string
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: ''
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
+import style from './layout.css'
 import Header from './header'
 
 const Layout = ({ children }) => {
