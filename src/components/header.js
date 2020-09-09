cb366c3b976b43dffff3ae0c6a350cbf74771834
Tibor Leupold <tibor@lpld.io>
2020-09-08 15:19:00 -1000

    Add logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,32 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
-import style from './header.module.css'
+import _ from './header.module.css'
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/#projects'>Projects</Link></li>
-        <li><Link to='/#contact'>Contact</Link></li>
-      </ul>
-    </nav>
-  </header>
-)
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      logo: file(name: {eq: "lpld-logo"}) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <header>
+      <svg width='48' height='48' xmlns='http://www.w3.org/2000/svg'>
+        <image src={data.logo.publicURL} />
+      </svg>
+      <nav>
+        <ul>
+          <li><Link to='/#projects'>Projects</Link></li>
+          <li><Link to='/#contact'>Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string
