f71465632e1547ebcfdb8a0d0b49e5862b48ba1b
Tibor Leupold <tibor@lpld.io>
2020-09-09 17:30:39 -1000

    Fix vertical centering of footer text
    It was geometrically centered.. but it did not look right. This must have someting
    to do with how Helvetica shoots above and below. Because there is almost no letters
    shooting down, the text appeared to be too far up.
    
    This is now compensated with different padding above and below the copyright
    text.

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,9 +5,7 @@ import './footer.module.css'
 const Footer = ({ author }) => {
   return (
     <footer>
-      <div>
-        © {new Date().getFullYear()}, {author}
-      </div>
+      © {new Date().getFullYear()}, {author}
     </footer>
   )
 }
