99c39950eb893f29a7bc744fc9ea447a1705f447
Tibor Leupold <tibor@lpld.io>
2020-09-10 16:15:56 -1000

    Move social links list to separate component

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,54 +1,16 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import { faLinkedin, faTwitter, faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
 
 import style from './contact.module.css'
 
 import ContactButton from './contact-button'
-import SocialLinkIcon from './social-link'
+import SocialLinks from './social-links-list'
 
 const Contact = () => {
-  const data = useStaticQuery(graphql`
-    query SocialLinksQuery {
-      site {
-        siteMetadata {
-          socialLinks {
-            linkedin
-            twitter
-            github
-            stackoverflow
-          }
-        }
-      }
-    }
-  `)
-  const icons = {
-    linkedin: faLinkedin,
-    twitter: faTwitter,
-    github: faGithub,
-    stackoverflow: faStackOverflow
-  }
-  const socialLinks = data.site.siteMetadata.socialLinks
-  const socialLinkElements = Object.keys(socialLinks).map((social) => {
-    const url = socialLinks[social]
-    if (url) {
-      return (
-        <li>
-          <SocialLinkIcon icon={icons[social]} label={social} url={url} />
-        </li>
-      )
-    } else {
-      return null
-    }
-  })
-
   return (
     <section id='contact' className={style.contact}>
       <h2>Let's build something together</h2>
       <ContactButton />
-      <ul className={style.socialLinksList}>
-        {socialLinkElements}
-      </ul>
+      <SocialLinks />
     </section>
   )
 }
---
+++ src/components/social-links-list.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { faLinkedin, faTwitter, faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+
+import style from './social-links-list.module.css'
+import SocialLinkIcon from './social-link'
+
+const SocialLinks = () => {
+  const data = useStaticQuery(graphql`
+    query SocialLinksQuery {
+      site {
+        siteMetadata {
+          socialLinks {
+            linkedin
+            twitter
+            github
+            stackoverflow
+          }
+        }
+      }
+    }
+  `)
+  const icons = {
+    linkedin: faLinkedin,
+    twitter: faTwitter,
+    github: faGithub,
+    stackoverflow: faStackOverflow
+  }
+  const socialLinks = data.site.siteMetadata.socialLinks
+  const socialLinkElements = Object.keys(socialLinks).map((social) => {
+    const url = socialLinks[social]
+    if (url) {
+      return (
+        <li>
+          <SocialLinkIcon icon={icons[social]} label={social} url={url} />
+        </li>
+      )
+    } else {
+      return null
+    }
+  })
+
+  return (
+    <ul className={style.socialLinksList}>
+      {socialLinkElements}
+    </ul>
+  )
+}
+
+export default SocialLinks
