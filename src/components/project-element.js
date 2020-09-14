184e1caf7a8dc23c595f7079e55cca2c19bd1098
Tibor Leupold <tibor@lpld.io>
2020-09-14 13:26:14 -1000

    Make project link have content
    This is necessary for accessibility reasons. Before, I did strech the whole link.
    To be able to set the link on an actual element (I am using the project name element)
    and still strech it, I need to stretch the `::after` pseudo element.

--- src/components/project-element.js
+++ src/components/project-element.js
@@ -5,10 +5,12 @@ import style from './project-element.module.css'
 const ProjectElement = ({ projectName, description, url, image }) => {
   return (
     <div className={style.projectElement}>
-      <a href={url} className={style.projectLink} />
+
       {image || null}
       <div className={style.projectData}>
-        <h3 className={style.projectName}>{projectName}</h3>
+        <a href={url} className={style.projectLink}>
+          <h3 className={style.projectName}>{projectName}</h3>
+        </a>
         <p className={style.projectDescription}>{description}</p>
       </div>
     </div>
