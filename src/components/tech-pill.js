7ae44d1af127007803cb33ec942b7c6bd0ac7763
Tibor Leupold <tibor@lpld.io>
2020-09-15 12:25:36 -1000

    Remove empty tech pills

--- src/components/tech-pill.js
+++ src/components/tech-pill.js
@@ -3,6 +3,10 @@ import React from 'react'
 import style from './tech-pill.module.css'
 
 const TechPill = ({ name }) => {
+  name = name.trim()
+  if (name === '') {
+    return null
+  }
   return (
     <span className={style.techPill}>{name.toLowerCase()}</span>
   )
