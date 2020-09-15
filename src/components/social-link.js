2805ab393f3d2f874d41cc5e5777c611e0b2bdfc
Tibor Leupold <tibor@lpld.io>
2020-09-15 12:33:34 -1000

    Add text link style

--- src/components/social-link.js
+++ src/components/social-link.js
@@ -2,11 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import './social-link.module.css'
+import style from './social-link.module.css'
 
 const SocialIconLink = ({ icon, url, label }) => {
   return (
-    <a href={url} aria-label={label} target='_blank' rel='noopener noreferrer'>
+    <a className={style.socialLink} href={url} aria-label={label} target='_blank' rel='noopener noreferrer'>
       <FontAwesomeIcon icon={icon} />
     </a>
   )
